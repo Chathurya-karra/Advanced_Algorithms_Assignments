3b86783cd989ccbc2f008be37c794d6d6d3aab37
Chathurya-karra <116915204+Chathurya-karra@users.noreply.github.com>
2022-11-06 22:39:45 -0600

    Add files via upload

---
+++ Array_sparsematrix.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // sparse matrix of class 4x5 with 6 non-zero values
+    int sparseMatrix[4][5] =
+    {
+        {0 , 0 , 3 , 0 , 4 },
+        {0 , 0 , 5 , 7 , 0 },
+        {0 , 0 , 0 , 0 , 0 },
+        {0 , 2 , 6 , 0 , 0 }
+    };
+
+    // Finding total non-zero values in the sparse matrix
+    int size = 0;
+    for (int row = 0; row < 4; row++)
+        for (int column = 0; column < 5; column++)
+            if (sparseMatrix[row][column] != 0)
+                size++;
+
+    // Defining result Matrix
+    int resultMatrix[3][size];
+
+    // Generating result matrix
+    int k = 0;
+    for (int row = 0; row < 4; row++)
+        for (int column = 0; column < 5; column++)
+            if (sparseMatrix[row][column] != 0)
+            {
+                resultMatrix[0][k] = row;
+                resultMatrix[1][k] = column;
+                resultMatrix[2][k] = sparseMatrix[row][column];
+                k++;
+            }
+
+    // Displaying result matrix
+    cout<<"Triplet Representation : "<<endl;
+    for (int row=0; row<3; row++)
+    {
+        for (int column = 0; column<size; column++)
+            cout<<resultMatrix[row][column]<<" ";
+
+        cout<<endl;
+    }
+    return 0;
+}
